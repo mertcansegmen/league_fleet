486b4a90d4cd8c44ada4dc70608ea6a1c38a319a
mertcansegmen <mertcan.segmen@hotmail.com>
2018-05-01 21:54:24 +0300

    some update

--- app/src/main/java/com/example/mert/leagueoflegendsliginne/MainActivity.java
+++ app/src/main/java/com/example/mert/leagueoflegendsliginne/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.mert.leagueoflegendsliginne;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
